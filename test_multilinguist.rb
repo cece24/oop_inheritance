560d9224c2822f69fc9ca7befc7d45dd66790422
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-09 16:50:03 -0500

    Write unit test for language_in method

--- test_multilinguist.rb
+++ test_multilinguist.rb
@@ -3,5 +3,14 @@ require 'minitest/pride'
 require './multilinguist.rb'
 
 class TestMultilinguist < MiniTest::Test
-  
+  def setup
+    @multilinguist = Multilinguist.new
+  end
+
+  def test_language_in_returns_correct_language
+    expected = 'fr'
+    result = @multilinguist.language_in('France')
+    assert_equal(expected, result)
+  end
+
 end
